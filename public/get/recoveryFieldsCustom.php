0cf9f6890b96e65144c1591b6c53c48d98c12058
Edinei J. Bauer <galera.org@gmail.com>
2020-05-08 17:49:14 -0300

    recovery fixed

--- public/get/recoveryFieldsCustom.php
+++ public/get/recoveryFieldsCustom.php
@@ -1,8 +1,8 @@
 <?php
 
-$type = $link->getVariaveis()[0];
-$entity = $link->getVariaveis()[1];
+$entity = $link->getVariaveis()[0];
+$type = $link->getVariaveis()[1];
 
 $data['data'] = [];
 if(file_exists(PATH_HOME . "_cdn/fieldsCustom/{$entity}/{$type}/" . $_SESSION['userlogin']['id'] . ".json"))
-    $data['data'] = json_decode(file_get_contents(PATH_HOME . "_cdn/fieldsCustom/{$entity}/{$type}" . $_SESSION['userlogin']['id'] . ".json"), !0);
+    $data['data'] = json_decode(file_get_contents(PATH_HOME . "_cdn/fieldsCustom/{$entity}/{$type}/" . $_SESSION['userlogin']['id'] . ".json"), !0);
