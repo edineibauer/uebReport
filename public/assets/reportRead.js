72a784a4ca965618aad7f699b89d5a11ed26c6f5
Edinei J. Bauer <galera.org@gmail.com>
2020-05-28 17:15:33 -0300

    add search on readOffline

--- public/assets/reportRead.js
+++ public/assets/reportRead.js
@@ -50,7 +50,7 @@ async function reportRead(entity, search, filter, aggroup, soma, media, maior, m
                 if (dados.response === 1)
                     resolve({data: dados.data.data, length: dados.data.total});
             },
-            error: () => resolve(readOffline(data, filter, order, reverse, limit, offset)),
+            error: () => resolve(readOffline(data, search, filter, order, reverse, limit, offset)),
             dataType: "json"
         })
     })
