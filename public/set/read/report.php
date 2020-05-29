ba87b25a442939930cb8ea157b747d4f5658381c
Edinei J. Bauer <galera.org@gmail.com>
2020-05-29 17:32:43 -0300

    report read without permission

--- public/set/read/report.php
+++ public/set/read/report.php
@@ -1,7 +1,5 @@
 <?php
 
-use \Config\Config;
-
 $filter['search'] = filter_input(INPUT_POST, 'search', FILTER_DEFAULT);
 $filter['regras'] = json_encode(filter_input(INPUT_POST, 'filter', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY));
 $filter['entidade'] = filter_input(INPUT_POST, 'entity', FILTER_DEFAULT);
@@ -23,15 +21,5 @@ $limit = filter_input(INPUT_POST, 'limit', FILTER_VALIDATE_INT);
 $limit = empty($limit) ? LIMITOFFLINE : $limit;
 $offset = filter_input(INPUT_POST, 'offset', FILTER_VALIDATE_INT);
 
-$setor = !empty($_SESSION['userlogin']) ? $_SESSION['userlogin']['setor'] : "0";
-$permissoes = Config::getPermission();
-
-/**
- * Estou logado, não sou ADM, não tenho permissão de leitura, mas a entidade é o meu tipo de usuário
- */
-$entityIsMySetor = ($setor !== "admin" && (isset($permissoes[$setor][$entity]['read']) && !$permissoes[$setor][$entity]['read']) && $setor !== "0" && $entity === $setor);
-
-if ($setor === "admin" || (isset($permissoes[$setor][$entity]['read']) || $permissoes[$setor][$entity]['read']) || $entityIsMySetor) {
-    $report = new \Report\Report($filter, $limit, $offset);
-    $data['data'] = ['data' => $report->getResult(), 'total' => $report->getTotal()];
-}
+$report = new \Report\Report($filter, $limit, $offset);
+$data['data'] = ['data' => $report->getResult(), 'total' => $report->getTotal()];
