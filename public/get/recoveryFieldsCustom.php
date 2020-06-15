359e2ddbbdada42c17026f0e1f62d49bd5bc44ee
Edinei J. Bauer <galera.org@gmail.com>
2020-06-14 22:17:30 -0300

    move recovery to config

--- public/get/recoveryFieldsCustom.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$entity = $link->getVariaveis()[0];
-$type = $link->getVariaveis()[1];
-
-$data['data'] = [];
-if(file_exists(PATH_HOME . "_cdn/fieldsCustom/{$entity}/{$type}/" . $_SESSION['userlogin']['id'] . ".json"))
-    $data['data'] = json_decode(file_get_contents(PATH_HOME . "_cdn/fieldsCustom/{$entity}/{$type}/" . $_SESSION['userlogin']['id'] . ".json"), !0);
