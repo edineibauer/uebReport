097615fcc8a5f5fbe6bcd0af5750aff56c895173
Edinei J. Bauer <edineibauer@gmail.com>
2020-11-17 16:52:56 -0300

    fix relation info

--- public/view/UIRelatorios/UIRelatorios.php
+++ public/view/UIRelatorios/UIRelatorios.php
@@ -9,19 +9,18 @@
 
 <div class="main report-main animate-left">
     <div id="dashboard" class="container padding-xlarge s-padding-medium row">
-        <div class="col relative">
-            <header class="container s-padding-small">
-                <h5 class="left padding-32 s-padding-12 s-padding-tiny">
-                    <b>
-                        <i class="material-icons left padding-right">assessment</i>
-                        <span class="left">Relatórios</span>
-                    </b>
-                </h5>
-            </header>
-            <div class="col s12 m9 padding-small config-panel"></div>
-        </div>
         <div id="report" class="container">
-
+            <div class="col relative">
+                <header class="container s-padding-small">
+                    <h5 class="left padding-32 s-padding-12 s-padding-tiny">
+                        <b>
+                            <i class="material-icons left padding-right">assessment</i>
+                            <span class="left">Relatórios</span>
+                        </b>
+                    </h5>
+                </header>
+                <div class="col s12 m9 padding-small config-panel"></div>
+            </div>
         </div>
     </div>
 </div>
