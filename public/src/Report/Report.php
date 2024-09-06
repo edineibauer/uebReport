5d50364a972569de248ff85d1eecc2fc351bf361
Edinei J. Bauer <galera.org@gmail.com>
2024-09-06 18:05:37 -0300

    ajustar lógica de sistema para permitir consultar apenas registros filhos pertencentes ao sistema pai

--- public/src/Report/Report.php
+++ public/src/Report/Report.php
@@ -253,26 +253,34 @@ class Report
          * restringe leitura a somente dados do system_id de acesso
          * Aplica regra recursiva sobre sistema pai
          */
-        if ($_SESSION["userlogin"]["setor"] !== "admin" && !empty($info['system'])) {
-
-            // permite registros que não tem vinculos com nenhum sistema (criados pelo admin)
-            $queryLogic .= ($queryLogic !== "WHERE" ? " AND " : " ") . "(({$this->report['entidade']}.system_id IS NULL AND {$this->report['entidade']}.system_entity IS NULL)";
+        if ($_SESSION["userlogin"]["setor"] !== "admin" && !empty($_SESSION["userlogin"]["system_id"])) {
 
             /**
              * Se não for um administrador e
              * tem um sistema vinculado
              */
-            if(!empty($_SESSION["userlogin"]["system_id"])) {
-                $mySystem = Metadados::getInfo($_SESSION["userlogin"]['setor']);
+            $mySystem = Metadados::getInfo($_SESSION["userlogin"]['setor']);
 
-                //permite registros que estão vinculados ao meu sistema
-                $queryLogic .= " OR ({$this->report['entidade']}.system_id = {$_SESSION["userlogin"]["system_id"]} AND {$this->report['entidade']}.system_entity = '{$mySystem['system']}')";
+            // pega registros criado pelo meu sistema atual logado
+            $queryLogic .= ($queryLogic !== "WHERE" ? " AND " : " ") . "(({$this->report['entidade']}.system_id = {$_SESSION["userlogin"]["system_id"]} AND {$this->report['entidade']}.system_entity = '{$mySystem['system']}')";
+
+            if(!empty($info['system'])) {
+                //os registros estão vinculados a um sistema específico, hierarquia de gestão se aplica nesse caso
 
                 //permite registros que estão abaixo do meu sistema
                 $listaEntitySystemBelow = $this->_getEntitySystemBelow($mySystem['system'], $info['system'], []);
                 if(!empty($listaEntitySystemBelow)) {
-                    foreach ($listaEntitySystemBelow as $systemBelow)
-                        $queryLogic .= " OR {$this->report['entidade']}.system_entity = '" . $systemBelow . "'";
+                    foreach ($listaEntitySystemBelow as $systemBelow) {
+                        $read = new Read();
+                        $read->exeRead($systemBelow, "WHERE system_id = :s", ["s" => $_SESSION["userlogin"]["system_id"]]);
+                        if($read->getResult()) {
+                            $listaSistemasFilhos = [];
+                            foreach ($read->getResult() as $itemm)
+                                $listaSistemasFilhos[] = $itemm['id'];
+
+                            $queryLogic .= " OR ({$this->report['entidade']}.system_id IN(" . implode(',', $listaSistemasFilhos) . ") AND {$this->report['entidade']}.system_entity = '" . $systemBelow . "')";
+                        }
+                    }
                 }
             }
 
